3cd4d7c4cbb4c4ce1889e613aab683468c049968
Filippo Fratta Pasini <filippo.frattapasini@gmail.com>
2022-01-30 15:07:25 +0100

    merge art

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -13,6 +13,8 @@ namespace Kawaiiju
         [SerializeField] private ItemButton m_ItemTwo;
         [SerializeField] private ItemButton m_ItemThree;
 
+        [SerializeField] private RectTransform m_Menu;
+        
         [Header("Debug UI")]
         [SerializeField] private Button m_IncreaseBarValue;
         [SerializeField] private Slider m_NeedsBar;
@@ -32,6 +34,8 @@ namespace Kawaiiju
             itemsThrower.OnUpdateButtonOne_AddCallback(m_ItemOne.UpdateButtonIcon);
             itemsThrower.OnUpdateButtonTwo_AddCallback(m_ItemTwo.UpdateButtonIcon);
             itemsThrower.OnUpdateButtonThree_AddCallback(m_ItemThree.UpdateButtonIcon);
+            
+            m_Menu.gameObject.SetActive(true);
         }
 
         private void Start()
